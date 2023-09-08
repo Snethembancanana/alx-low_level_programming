aa76e5b19c933444d748cc2a3cd22b7558059273
Snethemba <ncananasnethemba@gmail.com>
2023-09-07 20:29:02 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include<stdio.>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle"); 
+	retuen (0);
+}	
