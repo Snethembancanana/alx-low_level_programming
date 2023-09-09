a19e54f9f370639cd189e21a87a63e37ffe2387e
Snethemba <ncananasnethemba@gmail.com>
2023-09-09 12:10:48 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always return (0)
+ */
+
+int main(void)
+{
+	int x;
+
+	x = 'z';
+	while
+		(x <= 'z') {
+			putchar(x);
+			x--;
+		}
+	putchar('\n');
+	return (0);
+}
+
