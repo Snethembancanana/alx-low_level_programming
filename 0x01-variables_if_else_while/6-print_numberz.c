48ff37167b892b93465ed277f3193d239a19a0c1
Snethemba <ncananasnethemba@gmail.com>
2023-09-09 11:50:17 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always return (0)
+ */
+
+int main(void)
+{
+	int n;
+
+	while
+	       	(n < 10) {
+		putchar(n + '0');
+		n++;
+		}
+	putchar('\n');
+	return (0);
+}
