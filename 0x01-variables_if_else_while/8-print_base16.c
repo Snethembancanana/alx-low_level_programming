a39511a05bb4ed6803aa560b0640824e3fede297
Snethemba <ncananasnethemba@gmail.com>
2023-09-09 13:59:17 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always return (0)
+ */
+
+int main(void)
+{
+	char c;
+
+	int n;
+
+	c = 'a';
+	n = 0;
+	while
+		(n < 10) {
+			putchar(n + '0');
+			n++;
+		}
+	while
+		(c <= 'f') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
