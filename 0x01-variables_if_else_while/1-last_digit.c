365a87a7a4e9c1b5c7dcb50f77cb540db65cc460
Snethemba <ncananasnethemba@gmail.com>
2023-09-08 21:01:33 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,17 +8,23 @@
  */
 int main(void)
 {
-	int n;
-	int y;
+	int n, y;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	y = n % 10;
+
 	if (y > 5)
+	{
 	       printf("Last digit of %d is %d and is greater than 5\n", n, y);
-	if (y == 0)
+	}
+	else if (y == 0)
+	{
 		printf("Last digit of %d is %d and is 0\n", n, y);
+	}
 	if (y < 6 && y != 0)
+	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, y);
+	}
 	return (0);
 }
