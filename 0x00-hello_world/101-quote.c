548525d19b657e24d60ad4c0fd3adf17269fb0aa
Snethemba <ncananasnethemba@gmail.com>
2023-09-10 12:58:56 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: program should return (1)
+ */
+
+int main(void)
+{
+	fputs("and that piece of are is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+       return (1);
+}	       
