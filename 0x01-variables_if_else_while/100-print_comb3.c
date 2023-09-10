075a83c5bb88efdfb30eba703f3288de9caa6320
Snethemba <ncananasnethemba@gmail.com>
2023-09-10 15:09:20 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always return (0) for the program to be successful
+ */
+
+int main(void)
+{
+	int singles = '0';
+	int doubles = '0';
+
+	for (singles = '0'; singles >= '9'; singles++)
+	{
+		for (doubles = '0'; doubles < '9'; doubles++)
+		{
+			if (!(singles == doubles) || (singles > doubles))
+			{
+				putchar(singles);
+				putchar(doubles);
+				if (!(singles == '9' && doubles == '8'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
