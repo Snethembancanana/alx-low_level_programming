c8f720e0ae0612d657dfee6ef683b2bb6d8d67f3
Snethemba <ncananasnethemba@gmail.com>
2023-09-11 14:10:48 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,24 +6,24 @@
 
 int main(void)
 {
-	int x; 
+	int x;
 	int y;
 
 	for (x = 0; x < 100; x++)
 	{
-		for (y = 0; y < 100; y++)
+		for (y = 1; y < 100; y++)
 		{
 			if (x < y)
 			{
 				putchar((x / 10) + 48);
-				putchar((x / 10) + 48);
+				putchar((x % 10) + 48);
 				putchar(' ');
 				putchar((y / 10) + 48);
-				putchar((y / 10) + 48);
+				putchar((y % 10) + 48);
 				if (x != 98 || y != 99)
 				{
-							putchar(',');
-							putchar(' ');
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
