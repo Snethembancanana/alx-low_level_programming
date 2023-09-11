61318166de729b10392ce294ff3e4db8fa976bbd
Snethemba <ncananasnethemba@gmail.com>
2023-09-11 10:54:08 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,22 +6,22 @@
 
 int main(void)
 {
-	int a;
-	int b;
-	int c;
+	int n;
+	int m;
+	int l;
 
-	if (a = 48; a < 58; a++)
+	for (n = 48; n < 58; n++)
 	{
-		if (b = 49; b < 58; b++)
+		for (m = 49; m < 58; m++)
 		{
-			if (c = 50; c < 58; c++)
+			for (l = 50; l < 58; l++)
 			{
-				if (a < b && b < c)
+				if (l > m && m > n)
 				{
-					putchar(a);
-					putchar(b);
-					putchar(c);
-					if (!(a == 55 && b == 56 ))
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
 					{
 						putchar(',');
 						putchar(' ');
