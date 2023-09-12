4ce91a37ea1301eda33e4dd83d94f3b79e347a48
Snethemba <ncananasnethemba@gmail.com>
2023-09-12 15:15:38 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabets in lowercase
+ */
+
+void print_alphabet(void)
+
+{
+	char characters;
+
+	for (characters = 'a'; characters <= 'z'; characters++);
+	_putchar(characters);
+	_putchar('\n');
+}
+
