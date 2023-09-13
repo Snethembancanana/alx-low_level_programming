3ef1b3f4e67baa97c3d57df627a6151dd44a1aee
Snethemba <ncananasnethemba@gmail.com>
2023-09-13 15:48:29 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - function that adds two integers
+ * @num1: first integer
+ * @num2: second integer
+ * Return: return (0).
+ */
+
+int add(int num1, int num2)
+{
+	int outcome;
+
+	outcome = num1 + num2;
+
+		return (outcome);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * times_table - function that prints the 9 times table
+ */
+
+void times_table(void)
+{
+	int vert;
+	int hori;
+	int outcome;
+	int modulus;
+	int whole;
+
+	for (vert = 0; vert <= 9; vert++)
+	{
+		for (hori = 0; hori <= 9; hori++)
+		{
+			outcome = vert * hori;
+			modulus = outcome % 10;
+			whole = outcome / 10;
+
+			if (hori == 0)
+			{
+				_putchar('0');
+			}
+			else if (outcome < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(modulus + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(modulus + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
+
