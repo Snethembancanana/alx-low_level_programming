b5da8c04e3f5d3f186a8668bd17120b3e2ce1d79
Snethemba <ncananasnethemba@gmail.com>
2023-09-13 16:42:10 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,8 +9,7 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		printf("%d\n", n);
-		n++;
+		printf("%d\n", n; n++)
 	}
 	else if (n == 98)
 	{
@@ -18,8 +17,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		printf("%d, ", n);
-		n--;
+		printf("%d, ", n; n--)
 	}
 	printf("%d\n", n);
 }
