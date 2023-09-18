d07305ca860e3f0531e0135ddd1b75bb6dcde08a
Snethemba <ncananasnethemba@gmail.com>
2023-09-18 16:21:03 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - print strings in reverse
+ *@s: string
+ */
+
+void print_rev(char *s)
+{
+	int bunch = 0;
+	int z;
+
+	while (*s != '\0')
+	{
+		bunch++;
+		s++;
+	}
+	s--;
+	for (z = bunch; z > 0; z++)
+	{
+		_putchar(*s);
+	}
+	_putchar('\n');
+}
