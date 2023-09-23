4dc56b45d323189fed84c187f3fb5a6db6c21fbd
Snethemba <ncananasnethemba@gmail.com>
2023-09-22 20:50:33 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encode a string
+ * @n: input value
+ * Return: retun (n)
+ */
+
+char *rot13(char *n)
+{
+	int i, j;
+	char s1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char s2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; s1[j] != '\0'; j++)
+		{
+			if (n[i] == s1[j])
+			{
+				n[i] = s2[j];
+				break;
+			}
+		}
+	}
+	return (n);
+}
