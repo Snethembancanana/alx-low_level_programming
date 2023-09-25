019dedffad32663aa939899201e7c76009038190
Snethemba <ncananasnethemba@gmail.com>
2023-09-25 16:51:06 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,28 +10,43 @@ void print_times_table(int n)
 	int b;
 	int c;
 
-	for (n >= 0 && n <= 15)
+	if (n >= 0 && n <= 15)
 	{
 		for (a = 0; a <= n; a++)
 		{
-			_putchar(',');
-			_putchar(' ');
-			c = a * b;
+			for (b = 0; b <= n; b++)
+			{
+				c = b * a;
+				if (b == 0)
+				{
+					_putchar(c + '0');
+				}
+				else if (c < 10 && b != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(c + '0');
+				}
+				else if (c >= 10 && c < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((c / 10) + '0');
+					_putchar((c % 10) + '0');
+				}
+				else if (c >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((c / 100) + '0');
+					_putchar(((c / 10) % 10) + '0');
+					_putchar((c % 10) + '0');
+				}
+			}
+			_putchar('\n');
 		}
-		if (c <= 99)
-			_putchar(' ');
-		if (c <= 9)
-			_putchar(' ');
-		if (c >= 100)
-		{
-			_putchar((c / 100) + '0');
-			_putchar((c / 10) % 10 + '0');
-		}
-		else if (c <= 99 && c >= 10)
-		{
-			_putchar((c / 10) + '0');
-		}
-		_putchar((c % 10) + '0');
 	}
-	_putchar('\n');
 }
