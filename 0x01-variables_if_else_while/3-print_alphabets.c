bb1212cee550bde5736dfcf03710b6bbd25180c0
Snethemba <ncananasnethemba@gmail.com>
2023-09-25 12:33:57 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,18 +9,18 @@ int main(void)
 	char c;
 	char d;
 
-	c = 'a'
-	d = 'A'
-	while
-		(c <= 'z') {
-			putchar(c);
-			c++;
-		}
-	while
-		(d <= 'z') {
-			putchar (d);
-			d++;
-		}
+	c = 'a';
+	d = 'A';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (d <= 'Z')
+	{
+		putchar (d);
+		d++;
+	}
 	putchar('\n');
 	return (0);
 }
